53018348c5f543a9a56cab9fbe5878eb3805f44c
Rafael Santos <rsanthos@gmail.com>
2017-08-09 17:08:21 -0300

    route to get token nd callback

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,31 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $query = http_build_query([
+        'client_id' => 3,
+        'redirect_uri' => 'http://localhost:9999/callback',
+        'response_type' => 'code',
+        'scope' => ''
+    ]);
+
+    return redirect("http://localhost:8000/oauth/authorize?$query");
+});
+
+Route::get('/callback', function (\Illuminate\Http\Request $request) {
+    $code = $request->get('code');
+
+    $http = new \GuzzleHttp\Client();
+    $response = $http->post('http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'client_id' => 3,
+            'client_secret' => 'tq11dq55jw1t7A7eMP3ZHXN9bsl4LcfWNi8Ua35m',
+            'redirect_uri' => 'http://localhost:9999/callback',
+            'code' => $code,
+            'grant_type' => 'authorization_code'
+        ]
+    ]);
+
+    dd(json_decode($response->getBody(), true));
 });
 
 Auth::routes();
